adfd05cf32502b0831ead80a3ebc57d61dc92f8e
Patrick Quilty <pquilty79@gmail.com>
2022-02-18 14:50:08 -0600

    added functions

--- movies.js
+++ movies.js
@@ -16,5 +16,49 @@ function fetchOneMovie(id) {
             .then(data => console.log(data)))
         .catch(err => console.log(err));
 }
+// fetchOneMovie(4)
 
-fetchOneMovie(4)
+function createMovie() {
+    const newMovie = {
+        title: "placeholder",
+        director: "placeholder",
+        actors: "placeholder",
+        genre: "placeholder",
+        year: "placeholder",
+        rating: "placeholder",
+        plot: "placeholder",
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch(`${url}`, options)
+        .then(res => {
+            console.log('New movie has been saved');
+        })
+        .catch(err => console.log(err));
+}
+
+// createMovie()
+
+function updateMovie(id) {
+    const movieUpdate = {
+        title: 'new title',
+    }
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(movieUpdate),
+    };
+    fetch(`${url}/${id}`, options)
+        .then(res => {
+            console.log(`Movie ${id} has been updated`)
+        })
+        .catch(err => console.log(err));
+}
+// updateMovie(4)
