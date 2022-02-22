d1006066afba3f75240c8db1c9cd89adeb8043c1
Patrick Quilty <pquilty79@gmail.com>
2022-02-22 14:36:08 -0600

    added search to main html

--- movies.js
+++ movies.js
@@ -93,3 +93,68 @@ function getData(movie){
     // console.log(`plot: ${movie.plot}`)
     return `<div>${movie.director}</div>`
 }
+
+
+let searchThis = "";
+let movieID = "";
+let movieTrailer = "";
+let movie = {
+    title: "",
+    director: "",
+    actors: "",
+    genre: "",
+    year: "",
+    rating: "",
+    plot: "",
+    poster: "",
+    imDbID: "",
+    trailerURL: "",
+}
+
+$("#addmovie").click(function (){
+    searchThis = $('#moviesearch').val()
+    searchIMDB()
+})
+
+function searchIMDB(){
+    fetch(`https://imdb-api.com/en/API/SearchMovie/${IMDb_TOKEN}/${searchThis}`)
+        .then(res => res.json()
+            .then(data => {
+                movieID = data.results[0].id
+                getMovieDetails()
+            }))
+
+        .catch(err => console.log(err));
+}
+
+function getMovieDetails(){
+    fetch(`https://imdb-api.com/en/API/Title/${IMDb_TOKEN}/${movieID}`)
+        .then(res => res.json()
+            .then(data => {
+                movie.title = data.title;
+                movie.director = data.directors;
+                movie.actors = data.stars;
+                movie.genre = data.genres;
+                movie.year = data.year;
+                movie.rating = parseInt(data.imDbRating)/2;
+                movie.plot = data.plot;
+                movie.poster = data.image;
+                movie.imDbID = data.id;
+                getTrailer().then(r => {
+                    movie.trailerURL = r
+                    console.log(movie)
+                    createMovie(movie)
+                })
+            }))
+        .catch(err => console.log(err));
+}
+
+function getTrailer(){
+    return fetch(`https://imdb-api.com/en/API/YouTubeTrailer/${IMDb_TOKEN}/${movieID}`)
+        .then(res => res.json()
+            .then(data => {
+                movieTrailer = data.videoUrl;
+                return movieTrailer
+            }))
+        .catch(err => console.log(err));
+}
