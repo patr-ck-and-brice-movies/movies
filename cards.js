5d0b3beb9bfa27405170d2ec913e22938f0cb9f1
Brice Ernst <brice.ernst1@gmail.com>
2022-02-23 12:32:05 -0600

    search and add css

--- cards.js
+++ cards.js
@@ -217,15 +217,15 @@ function getData(movie){
           </div>
           <div class="col2">
             <fieldset class="rating" id="rating-${movie.id}">
-              <input type="radio" id="star5" name="rating-${movie.rating}" value="5" /><label class="full" for="star5" title="Awesome - 5 stars"></label>
+              <input type="radio" id="star5" name="rating-${movie.rating}" value="5" checked/><label class="full" for="star5" title="Awesome - 5 stars"></label>
               <input type="radio" id="star4half" name="rating-${movie.rating}" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-              <input type="radio" id="star4" name="rating-${movie.rating}" value="4" checked /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+              <input type="radio" id="star4" name="rating-${movie.rating}" value="4"/><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
               <input type="radio" id="star3half" name="rating-${movie.rating}" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-              <input type="radio" id="star3" name="rating-${movie.rating}" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+              <input type="radio" id="star3" name="rating-${movie.rating}" value="3"/><label class = "full" for="star3" title="Meh - 3 stars"></label>
               <input type="radio" id="star2half" name="rating-${movie.rating}" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-              <input type="radio" id="star2" name="rating-${movie.rating}" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+              <input type="radio" id="star2" name="rating-${movie.rating}" value="2"/><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
               <input type="radio" id="star1half" name="rating-${movie.rating}" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-              <input type="radio" id="star1" name="rating-${movie.rating}" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+              <input type="radio" id="star1" name="rating-${movie.rating}" value="1"/><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
               <input type="radio" id="starhalf" name="rating-${movie.rating}" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
             </fieldset>
           </div>
