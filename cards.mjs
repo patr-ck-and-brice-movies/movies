fd059390bc792a069b10c2206f3307597514d78a
Patrick Quilty <pquilty79@gmail.com>
2022-02-23 11:07:50 -0600

    working animation

--- cards.mjs
+++ cards.mjs
@@ -1,41 +1,11 @@
 "use strict";
 
 
-
-
 const url = "https://thunder-outrageous-polka.glitch.me/movies"
-// // attempt at adding spinner
-// document.onload = function() {
-//     // import {Spinner} from "./spin.umd.js";
-//     const opts = {
-//         lines: 13, // The number of lines to draw
-//         length: 38, // The length of each line
-//         width: 17, // The line thickness
-//         radius: 45, // The radius of the inner circle
-//         scale: 1, // Scales overall size of the spinner
-//         corners: 1, // Corner roundness (0..1)
-//         speed: 1, // Rounds per second
-//         rotate: 0, // The rotation offset
-//         animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
-//         direction: 1, // 1: clockwise, -1: counterclockwise
-//         color: '#ffffff', // CSS color or array of colors
-//         fadeColor: 'transparent', // CSS color or array of colors
-//         top: '50%', // Top position relative to parent
-//         left: '50%', // Left position relative to parent
-//         shadow: '0 0 1px transparent', // Box-shadow for the lines
-//         zIndex: 2000000000, // The z-index (defaults to 2e9)
-//         className: 'spinner', // The CSS class to assign to the spinner
-//         position: 'absolute', // Element positioning
-//     };
-//     let spinner = new Spinner(opts).spin();
-//     let target = document.getElementById("moviedata");
-//     target.appendChild(spinner.el);
-//
-// }
 
-
-
-
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
 
 function fetchAllMovies() {
     fetch(url)
